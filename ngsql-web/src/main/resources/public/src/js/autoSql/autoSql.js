b9bd01e6bb852f5fc376bf60c548b1b870c0c8c9
kangjun <976387544@qq.com>
2020-07-29 13:58:12 +0800

    新增文件

--- ngsql-web/src/main/resources/public/src/js/autoSql/autoSql.js
+++ ngsql-web/src/main/resources/public/src/js/autoSql/autoSql.js
@@ -1,129 +1,30 @@
 require(['checkbox'], function (Checkbox) {
     $(function () {
-        // new Radio({
-        //     el: '#testRadio',
-        //     defaultValue: '1',
-        //     items: [
-        //         {
-        //             label: '男',
-        //             value: '1'
-        //         }, {
-        //             label: '女',
-        //             value: '2'
-        //         }
-        //     ]
-        // });
         const checkbox = new Checkbox({
             el: '#testCheckbox',//绑定的容器;必填
-            title: '性别',//标题;可选
+            title: '操作类型',//标题;可选
             type: 'radio',//类型,默认为radio,另可选填checkbox;可选
-            defaultValue: '1',//初始化时默认选中的选项,多个值用逗号分隔;可选
-            disabled: '1',//初始化时需要禁用的选项,填对应选项的value值，多个值用逗号分隔;可选
+            defaultValue: '',//初始化时默认选中的选项,多个值用逗号分隔;可选
+            disabled: '',//初始化时需要禁用的选项,填对应选项的value值，多个值用逗号分隔;可选
             items: [//内容,list<map>
                 {
                     className: '',//class;可选
-                    label: '男',//展示的选项;必填
+                    label: 'insert',//展示的选项;必填
                     value: '1'//选项对应的key,不可重复;必填
                 }, {
-                    label: '女',
+                    label: 'update',
                     value: '2'
                 }, {
-                    label: '无',
+                    label: 'delete',
                     value: '3'
+                }, {
+                    label: 'create',
+                    value: '4'
+                }, {
+                    label: 'alter',
+                    value: '5'
                 }
             ]
-        });
-        checkbox.on("change", function (data) {
-            console.log(data);
-        });
-        $(".radio>.getVersion").on('click', function () {
-            console.log(checkbox.version);
-        });
-        $(".radio>.testDisabled").on('click', function () {
-            checkbox.disabled();
-        });
-        $(".radio>.testDisabled1").on('click', function () {
-            checkbox.disabled('1');
-        });
-        $(".radio>.testEnable").on('click', function () {
-            checkbox.enable();
-        });
-        $(".radio>.testGet").on('click', function () {
-            console.log(checkbox.get());
-        });
-        $(".radio>.testEnable1").on('click', function () {
-            checkbox.enable('1');
-        });
-        $(".radio>.testDestroy").on('click', function () {
-            checkbox.destroy();
-        });
-        $(".radio>.testCheck").on('click', function () {
-            checkbox.check();
-        });
-        $(".radio>.testCheck1").on('click', function () {
-            checkbox.check('1');
-        });
-        $(".radio>.testUncheck").on('click', function () {
-            checkbox.uncheck();
-        });
-        $(".radio>.testUncheck1").on('click', function () {
-            checkbox.uncheck('1');
-        });
-        const checkbox2 = new Checkbox({
-            el: '#testCheckbox2',//绑定的容器;必填
-            title: '国家',//标题;可选
-            type: 'checkbox',//类型,默认为radio,另可选填checkbox;可选
-            defaultValue: '1',//初始化时默认选中的选项,多个值用逗号分隔;可选
-            disabled: '1',//初始化时需要禁用的选项,填对应选项的value值，多个值用逗号分隔;可选
-            items: [//内容,list<map>
-                {
-                    className: 'china',//class;可选
-                    label: '中国',//展示的选项;必填
-                    value: '1'//选项对应的key,不可重复;必填
-                }, {
-                    label: '日本',
-                    value: '2'
-                }, {
-                    label: '韩国',
-                    value: '3'
-                }
-            ]
-        });
-        checkbox2.on("change", function (data) {
-            console.log(data);
-        });
-        $(".checkbox>.getVersion").on('click', function () {
-            console.log(checkbox2.version);
-        });
-        $(".checkbox>.testDisabled").on('click', function () {
-            checkbox2.disabled();
-        });
-        $(".checkbox>.testDisabled1").on('click', function () {
-            checkbox2.disabled('1');
-        });
-        $(".checkbox>.testEnable").on('click', function () {
-            checkbox2.enable();
-        });
-        $(".checkbox>.testGet").on('click', function () {
-            console.log(checkbox2.get());
-        });
-        $(".checkbox>.testEnable1").on('click', function () {
-            checkbox2.enable('1');
-        });
-        $(".checkbox>.testDestroy").on('click', function () {
-            checkbox2.destroy();
-        });
-        $(".checkbox>.testCheck").on('click', function () {
-            checkbox2.check();
-        });
-        $(".checkbox>.testCheck1").on('click', function () {
-            checkbox2.check('1');
-        });
-        $(".checkbox>.testUncheck").on('click', function () {
-            checkbox2.uncheck();
-        });
-        $(".checkbox>.testUncheck1").on('click', function () {
-            checkbox2.uncheck('1');
         });
     })
 })
