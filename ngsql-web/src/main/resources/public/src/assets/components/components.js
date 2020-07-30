53207f98071c7280314ae43ab80a027ea5efd92c
kangjun <976387544@qq.com>
2020-07-30 15:16:29 +0800

    格式化

--- ngsql-web/src/main/resources/public/src/assets/components/components.js
+++ ngsql-web/src/main/resources/public/src/assets/components/components.js
@@ -1,16 +1,1 @@
-/**
- * 组件公共方法
- */
-define("components", ['jquery'], function (j) {
-    return j.extend(this.prototype, {
-        insertStyle: function (s) {
-            return (function (e) {
-                    const i = document, l = "appendChild", n = "styleSheet", a = i.createElement("style");
-                    a.type = "text/css";
-                    i.getElementsByTagName("head")[0][l](a);
-                    a[n] ? a[n].cssText = e : a[l](i.createTextNode(e));
-                }
-            )(s);
-        }
-    })
-});
+define("components",['jquery'],function(j){return j.extend(this.prototype,{insertStyle:function(s){return(function(e){const i=document,l="appendChild",n="styleSheet",a=i.createElement("style");i.getElementsByTagName("head")[0][l](a);a[n]?a[n].cssText=e:a[l](i.createTextNode(e))})(s)}})});
