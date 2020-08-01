e829142a0ce5121bbe6c0483115753f70b308019
kangjun <976387544@qq.com>
2020-08-01 18:37:09 +0800

    tab组件支持纵向排列

--- ngsql-web/src/main/resources/public/src/assets/components/tab/tab.js
+++ ngsql-web/src/main/resources/public/src/assets/components/tab/tab.js
@@ -1,7 +1,7 @@
 define('components/tab/tab.tpl', ['handlebars'], function (h) {
     return h.template({
         1: function (e, i, l, n, a) {
-            let s, t, r = i != null ? i : e.nullContext || {}, d = l.helperMissing, o = "function",
+            let t, r = i != null ? i : e.nullContext || {}, d = l.helperMissing, o = "function",
                 c = e.escapeExpression,
                 title = c((t = (t = l.title || (i != null ? i.title : i)) != null ? t : d, typeof t === o ? t.call(r, {
                     name: "title",
@@ -11,7 +11,7 @@ define('components/tab/tab.tpl', ['handlebars'], function (h) {
             return '<li title="' + title + '"><span>' + title + '</span></li>';
         },
         2: function (e, i, l, n, a) {
-            let s, t, r = i != null ? i : e.nullContext || {}, d = l.helperMissing, o = "function",
+            let t, r = i != null ? i : e.nullContext || {}, d = l.helperMissing, o = "function",
                 c = e.escapeExpression;
             return '<div class="' + c((t = (t = l.className || (i != null ? i.className : i)) != null ? t : d, typeof t === o ? t.call(r, {
                 name: "className",
@@ -22,14 +22,14 @@ define('components/tab/tab.tpl', ['handlebars'], function (h) {
         compiler: [7, ">4.0.0"],
         main: function (e, i, l, n, a) {
             let s, r = i != null ? i : e.nullContext || {};
-            return '<div class="tab-menus"><div class="tab-menu">\n' +
+            return '<div class="tab-menus ' + i.direction + '"><div class="tab-menu">\n' +
                 '        <ul class="tab-menu-bg">\n' +
                 '            <li class="tab-title-bg-blue"></li>\n' +
                 '            <li class="tab-title-bg-white"></li>\n' +
                 '        </ul>\n' +
                 '        <ul class="tab-menu-ul">' + (
                     (
-                        s = l.each.call(r, i != null ? i.items : i,
+                        s = l.each.call(r, i.items,
                             {
                                 name: "each",
                                 hash: {},
@@ -41,7 +41,7 @@ define('components/tab/tab.tpl', ['handlebars'], function (h) {
                     ) != null ? s : ""
                 ) + '</ul></div><div class="tab-content">' + (
                     (
-                        s = l.each.call(r, i != null ? i.items : i,
+                        s = l.each.call(r, i.items,
                             {
                                 name: "each",
                                 hash: {},
@@ -60,8 +60,7 @@ define('tab', ['jquery', 'eventTarget', 'handlebars', 'components/tab/tab.tpl', 
         "use static";
         const VERSION = "0.3.3";
         let r = function (h) {
-            j.extend(this.prototype, c.getEl.call(this, h));
-            this.orginTab = [];
+            j.extend(this.prototype, c.getEl.call(this, h), n.call(this));
             e.call(this);
             l.call(this);
             f.call(this);
@@ -76,7 +75,7 @@ define('tab', ['jquery', 'eventTarget', 'handlebars', 'components/tab/tab.tpl', 
                     index = j('.tab-menus>div.tab-content>div.' + t, $el).index() - 1;
                 } else if (typeof t === "number" || (typeof t === "string" && /^\d+$/.test(t)) && !isNaN(Number(t))) {
                     index = Number(t);
-                    if (index >= this.orginTab.length) {
+                    if (index > this.orginTab.length) {
                         return false;
                     }
                 } else {
@@ -89,11 +88,15 @@ define('tab', ['jquery', 'eventTarget', 'handlebars', 'components/tab/tab.tpl', 
                 }
             }
         });
+        const n = function () {
+            if (!!!this.options.direction || this.options.direction !== 'vertical') {
+                this.options.direction = "horizontal";
+            }
+            this.orginTab = [];
+        }
         const l = function () {
             this.$el.html(t(this.options));
-            const width = this.options.items.length * 150;
             const p = s.call(this);
-            j('.tab-menus>div.tab-menu>ul.tab-menu-ul', this.$el).css('width', width + 'px');
             i.call(this, p);
             j('.tab-menus>div.tab-menu>ul.tab-menu-ul>li:eq(' + p + ')', this.$el).addClass('checked');
         };
@@ -137,7 +140,7 @@ define('tab', ['jquery', 'eventTarget', 'handlebars', 'components/tab/tab.tpl', 
             });
 
             const that = this;
-            j('.tab-menus>div.tab-menu>ul.tab-menu-ul>li:not(.checked)', $el).one('click', function (e) {
+            j('.tab-menus>div.tab-menu>ul.tab-menu-ul>li:not(.checked)', $el).one('click', function () {
                 i.call(that, j(this).index());
             });
             $el.on('click', '.tab-menus>div.tab-menu>ul.tab-menu-ul>li', j.proxy(function (i) {
@@ -184,13 +187,39 @@ define('tab', ['jquery', 'eventTarget', 'handlebars', 'components/tab/tab.tpl', 
             m.call(this.$el, 'blue', d);
         };
         const m = function (b, i) {
-            j('.tab-menu-bg li.tab-title-bg-' + b, this).css('left', (i) * 150 + 'px');
+            const bg = j('.tab-menu-bg li.tab-title-bg-' + b, this);
+            if (this.children('.tab-menus').hasClass('horizontal')) {
+                bg.css('left', (i) * 150 + 'px');
+            } else if (this.children('.tab-menus').hasClass('vertical')) {
+                bg.css('top', (i) * 42 + 'px');
+            }
         };
         return r;
     }
 );
 require(['components'], function (c) {
-    c.insertStyle('        .tab-menus {\n' +
+    c.insertStyle(
+        '        .tab-menus.horizontal > div.tab-menu > ul {\n' +
+        '            height: 42px;\n' +
+        '            width: 100%;\n' +
+        '        }\n' +
+        '\n' +
+        '        .tab-menus.vertical > div.tab-menu > ul.tab-menu-bg {\n' +
+        '            height: inherit;\n' +
+        '            width: 150px;\n' +
+        '        }\n' +
+        '\n' +
+        '        .tab-menus.vertical > div.tab-menu > ul.tab-menu-ul {\n' +
+        '            width: 150px;\n' +
+        '        }\n' +
+        '\n' +
+        '        .tab-menus.vertical > div.tab-menu {\n' +
+        '            height: inherit;\n' +
+        '            float: left;\n' +
+        '        }\n' +
+        '\n' +
+        '        /**/\n' +
+        '        .tab-menus {\n' +
         '            margin: auto;\n' +
         '            position: absolute;\n' +
         '            width: inherit;\n' +
@@ -198,10 +227,11 @@ require(['components'], function (c) {
         '            border: 1px solid #009fe9;\n' +
         '        }\n' +
         '\n' +
-        '        .tab-menus>div.tab-menu>ul {\n' +
-        '            height: 42px;\n' +
-        '            width: 100%;\n' +
-        '            position: relative;\n' +
+        '        .tab-menus > div.tab-content {\n' +
+        '            height: 100%;\n' +
+        '        }\n' +
+        '\n' +
+        '        .tab-menus > div.tab-menu > ul {\n' +
         '            float: left;\n' +
         '            margin: 0;\n' +
         '            padding: 0;\n' +
@@ -209,37 +239,40 @@ require(['components'], function (c) {
         '            overflow: hidden;\n' +
         '        }\n' +
         '\n' +
-        '        .tab-menus>div.tab-menu>ul.tab-menu-bg {\n' +
+        '        .tab-menus > div.tab-menu > ul.tab-menu-bg {\n' +
         '            z-index: -1;\n' +
         '            background-color: #009fe977;\n' +
+        '            position: relative;\n' +
         '        }\n' +
         '\n' +
-        '        .tab-menus>div.tab-menu>ul.tab-menu-ul {\n' +
-        '            position: fixed;\n' +
+        '        .tab-menus > div.tab-menu > ul.tab-menu-ul {\n' +
+        '            position: absolute;\n' +
+
         '        }\n' +
         '\n' +
-        '        .tab-menus>.tab-menu>ul>li {\n' +
+        '        .tab-menus > div.tab-menu > ul > li {\n' +
         '            list-style: none;\n' +
         '            width: 140px;\n' +
         '            padding: 0 5px;\n' +
         '            float: left;\n' +
-        '            height: inherit;\n' +
+        '            height: auto;\n' +
         '        }\n' +
         '\n' +
-        '        .tab-menus>div.tab-menu>.tab-menu-ul > li {\n' +
+        '        .tab-menus > div.tab-menu > ul.tab-menu-ul > li {\n' +
         '            min-width: 100px;\n' +
         '            white-space: nowrap;\n' +
         '            overflow: hidden;\n' +
         '            text-overflow: ellipsis;\n' +
-        '            line-height: 40px;\n' +
+        '            line-height: 42px;\n' +
         '            text-align: center;\n' +
         '            cursor: pointer;\n' +
         '            color: #000;\n' +
         '        }\n' +
         '\n' +
-        '        .tab-menus>div.tab-menu>ul.tab-menu-bg>li {\n' +
+        '        .tab-menus > div.tab-menu > ul.tab-menu-bg > li {\n' +
         '            opacity: 1;\n' +
         '            left: 0;\n' +
+        '            height: 42px;\n' +
         '            position: absolute;\n' +
         '            -webkit-transition: all .5s;\n' +
         '            -moz-transition: all .5s;\n' +
@@ -247,31 +280,28 @@ require(['components'], function (c) {
         '            transition: all .5s\n' +
         '        }\n' +
         '\n' +
-        '        div.tab-menu>ul.tab-menu-bg>li.tab-title-bg-blue {\n' +
+        '        div.tab-menu > ul.tab-menu-bg > li.tab-title-bg-blue {\n' +
         '            background: #009fe9;\n' +
         '        }\n' +
         '\n' +
-        '        div.tab-menu>ul.tab-menu-bg>li.tab-title-bg-white {\n' +
+        '        div.tab-menu > ul.tab-menu-bg > li.tab-title-bg-white {\n' +
         '            background: #fff;\n' +
         '        }\n' +
         '\n' +
-        '        div.tab-menu>ul.tab-menu-bg>li.over {\n' +
+        '        div.tab-menu > ul.tab-menu-bg > li.over {\n' +
         '            opacity: 0;\n' +
         '        }\n' +
         '\n' +
-        '        div.tab-menu>ul.tab-menu-ul>li.over {\n' +
+        '        div.tab-menu > ul.tab-menu-ul > li.over {\n' +
         '            color: #fff;\n' +
         '        }\n' +
         '\n' +
-        '        .tab-menus > div.tab-content>div {\n' +
+        '        .tab-menus > div.tab-content > div {\n' +
         '            display: none;\n' +
         '        }\n' +
         '\n' +
-        '        .tab-menus >div.tab-menu{\n' +
+        '        .tab-menus > div.tab-content > div.show {\n' +
         '            display: block;\n' +
-        '        }\n' +
-        '\n' +
-        '        .tab-menus>div.tab-content>div.show {\n' +
-        '            display: block;\n' +
-        '        }\n');
+        '        }\n'
+    );
 })
--- ngsql-web/src/main/resources/public/src/js/test/test.js
+++ ngsql-web/src/main/resources/public/src/js/test/test.js
@@ -1,29 +1,115 @@
-require(['tab', 'checkbox'], function (Tab, Checkbox) {
+require(['tab','checkbox'], function (Tab,Checkbox) {
     $(function () {
-        $('.tab-menus li').each(function () {
-            $(this).mouseenter(function () {
-                const index = $(this).index();
-                move.call($('.tab-menu-bg li.tab-title-bg-blue'), index);
-            }).mousedown(function () {
-                const index = $(this).index();
-                $('.tab-menu-ul li.checked').removeClass("checked");
-                $(this).addClass("checked");
-                $('.tab-menus>.tab-context').removeClass('show').eq(index).addClass('show');
-                move.call($('.tab-menu-bg li.tab-title-bg-white'), index);
-            });
-        });
-        $('.tab-menus>ul').mouseleave(function () {
-            const index = $('.tab-menu-ul li.checked').index();
-            move.call($('.tab-menu-bg li.tab-title-bg-blue'), index);
-        });
 
-        function move(index) {
-            this.css('left', (index) * 150 + 'px');
-        }
+        // const move = function ($el) {
+        //     $('div.tab-menu>ul.tab-menu-ul li', $el).each(function () {
+        //         $(this).mouseenter(function () {
+        //             const index = $(this).index();
+        //             m.call($el, 'blue', index);
+        //             $('ul.tab-menu-ul li.over', $el).removeClass('over');
+        //             const tab = $('.tab-menu-ul li', $el).eq(index).addClass('over');
+        //             const bg = $('.tab-menu-bg li.tab-title-bg-white', $el);
+        //             if (tab.hasClass('checked')) {
+        //                 bg.addClass('over');
+        //             } else {
+        //                 bg.removeClass('over');
+        //             }
+        //         }).mousedown(function () {
+        //             w.call(this, $el);
+        //         });
+        //     });
+        //     $('div.tab-menu>ul.tab-menu-ul', $el).mouseleave(function () {
+        //         $('div.tab-menu>ul li.over', $el).removeClass('over');
+        //         const index = $('.tab-menu-ul li.checked', $el).index();
+        //         m.call($el, 'blue', index);
+        //     });
+        // }
+        // const m = function (b, i) {
+        //     const bg = $('.tab-menu-bg li.tab-title-bg-' + b, this);
+        //     if (this.hasClass('horizontal')) {
+        //         bg.css('left', (i) * 150 + 'px');
+        //     } else if (this.hasClass('vertical')) {
+        //         bg.css('top', (i) * 42 + 'px');
+        //     }
+        // };
+        // const w = function (e) {
+        //     $('ul.tab-menu-ul li.over', e).removeClass('over');
+        //     const index = $(this).index();
+        //     $('.tab-menu-ul li.checked', e).removeClass("checked");
+        //     $(this).addClass("checked");
+        //     $('div.tab-content>div', e).removeClass('show').eq(index).addClass('show');
+        //     m.call(e, 'white', index);
+        //     m.call(e, 'blue', index);
+        // }
+        //
+        // const $el = $('#testTab>.horizontal');
+        // const $el2 = $('#testTab2>.vertical');
+        // move.call(this, $el);
+        // move.call(this, $el2);
 
-        const tab = new Tab({
-            el: '#testTab',
+                const tab = new Tab({
+                    el: '#testTab',
+                    startPage: '2',
+                    items: [
+                        {
+                            title: 'tab1',
+                            className: 'tab',
+                            render: function () {
+                                return new Checkbox({
+                                    title: '性别',//标题;可选
+                                    type: 'radio',//类型,默认为radio,另可选填checkbox;可选
+                                    disabled: '1',//初始化时需要禁用的选项,填对应选项的value值，多个值用逗号分隔;可选
+                                    items: [//内容,list<map>
+                                        {
+                                            className: '',//class;可选
+                                            label: '男',//展示的选项;必填
+                                            value: '1'//选项对应的key,不可重复;必填
+                                        }, {
+                                            label: '女',
+                                            value: '2'
+                                        }, {
+                                            label: '未知',
+                                            value: '3'
+                                        }
+                                    ]
+                                }).$el;
+                            }
+                        },
+                        {
+                            title: 'tab2',
+                            className: 'tab2',
+                            render: function () {
+                                return "tab2";
+                            },
+                            click: function () {
+                                console.log("点击了tab2");
+                            }
+                        }, {
+                            title: 'tab3',
+                            render: function () {
+                                const a = $('<a href="javascript:">跳转到第二页</a>');
+                                a.on('click', function () {
+                                    tab.switchTab("1");
+                                })
+                                return a;
+                            }
+                        }, {
+                            title: 'tab4',
+                            render: function () {
+                                return "<div>\n" +
+                                    "                <img src=\"../../assets/img/link/home.ico\"/>\n" +
+                                    "            </div>";
+                            }
+                        }
+                    ]
+                });
+                tab.on('change', function () {
+                    console.log("切换了页签");
+                });
+        const tab2 = new Tab({
+            el: '#testTab2',
             startPage: '2',
+            direction:'vertical',
             items: [
                 {
                     title: 'tab1',
@@ -61,17 +147,21 @@ require(['tab', 'checkbox'], function (Tab, Checkbox) {
                 }, {
                     title: 'tab3',
                     render: function () {
-                        const a = $('<a href="javascript:">跳转到第二页</a>');
+                        const a = $('<a href="javascript:">跳转到第四页</a>');
                         a.on('click', function () {
-                            tab.switchTab("1");
+                            tab2.switchTab("3");
                         })
                         return a;
+                    }
+                }, {
+                    title: 'tab4',
+                    render: function () {
+                        return "<div>\n" +
+                            "                <img src=\"../../assets/img/link/home.ico\"/>\n" +
+                            "            </div>";
                     }
                 }
             ]
         });
-        tab.on('change', function () {
-            console.log("切换了页签");
-        })
     });
 })
